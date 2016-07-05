ac5d048e2cee09cfff2ee9f82fb65ad03523e587
西澤大地 <daichi2430@gmail.com>
2016-07-05 15:06:11 +0900

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,5 @@ require "csv"
 CSV.foreach('db/school.csv') do |row|
   School.create(name: row[1], enrollment_fee: row[2], materials_fee: row[3], pick_up_fee: row[4], scale: row[5], style: row[6], japanese_ratio: row[7], average_number: row[8], content: row[9], market_price_id: row[10])
 end
+
+ User.create(email: 'tech-camp@gmail.com', password: '12345678', nickname: 'Mako')
